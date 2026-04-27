7477b3e77ffbd010df5819973a1e978f4284c844
Gui Reis <gui.sreis25@gmail.com>
2026-04-27 10:40:46 -0300

    Add auto layout

--- _main/auxFrames.py
+++ _main/auxFrames.py
@@ -4,7 +4,7 @@
 
 ## Classe responsável pela criação dos widgets usado na Interface Gráfica
 
-#    Nessa classe é criada frames com label, entrada de texto e botão. SSão 
+#    Nessa classe é criada frames com label, entrada de texto e botão. São
 # usadas várias vezes dentro dá área de "Notas SAGE" e "Melhor Envio"
 
 
@@ -13,17 +13,13 @@
 from auxWidgets import AuxWidgets, QWidget
 
 # GUI:
-from PyQt6.QtWidgets import QFrame
+from PyQt6.QtWidgets import QFrame, QHBoxLayout
 
 
 class AuxFrame(QFrame, AuxWidgets):
-    ## Construtor: define a super classe e também a janela principal
-    def __init__(self, wid_:QWidget, nLb_:str, y_:int) -> None:                                 
+    ## Construtor: define a super classe
+    def __init__(self, nLb_:str) -> None:
         super(AuxFrame, self).__init__()
-
-        self.setGeometry(10, y_, 450, 20)
-        self.setParent(wid_)
-
         self.gui_Ui(nLb_)
 
     ## Destruidor: desaloca os atributos declarados
@@ -32,20 +28,25 @@ class AuxFrame(QFrame, AuxWidgets):
 
     ## Método: configura a frame
     def gui_Ui(self, nLb_:str) -> None:
-        self.lbl(nLb_, 10, 10, 0, 70, 20, self)                # Add label
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(2, 2, 2, 2)
 
-        self.txt = self.lineEdit(10, 80, 0, 330, 20, self)          # Add entrada de texto
+        layout.addWidget(self.lbl(nLb_, 10))                                           # Add label
 
-        self.but = self.btsImg(420, 0, 20, 20, self)                # Add botão
-        self.but.clicked.connect(self.bt_action)                    # Add ação do botão
+        self.txt = self.lineEdit(10)                                                    # Add entrada de texto
+        layout.addWidget(self.txt)
+
+        self.but = self.btsImg()                                                        # Add botão
+        self.but.clicked.connect(self.bt_action)                                       # Add ação do botão
+        layout.addWidget(self.but)
 
     ## Método: ação do botão
     def bt_action(self):
-        self.txt.selectAll()                                        # Seleciona o texto
-        self.txt.copy()                                             # Copia o texto
+        self.txt.selectAll()                                                            # Seleciona o texto
+        self.txt.copy()                                                                 # Copia o texto
 
     ## Método especial: pega o texto
-    def getText(self) -> str: return self.txt.text()                # Retorna o texto
+    def getText(self) -> str: return self.txt.text()                                    # Retorna o texto
 
     ## Método especial: define o texto
-    def setText(self, t_:str) -> None: self.txt.setText(t_)         # Define o texto
+    def setText(self, t_:str) -> None: self.txt.setText(t_)                            # Define o texto
