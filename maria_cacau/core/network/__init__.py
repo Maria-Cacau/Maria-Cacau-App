f091bb8e30faaa24d7bc73452675fe57e5656f4c
Gui Reis <gui.sreis25@gmail.com>
2026-05-13 22:33:11 -0300

    Update __init__.py

--- maria_cacau/core/network/__init__.py
+++ maria_cacau/core/network/__init__.py
@@ -3,3 +3,4 @@ from ._method import HTTPMethod
 from ._response import HTTPResponse
 from ._errors import NetworkError, HTTPRequestError, HTTPResponseError
 from ._config import configure, override, clear_override
+from ._client import LocalClient, HTTPClientContract
