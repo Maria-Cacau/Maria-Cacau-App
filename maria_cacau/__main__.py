4aa4b64a25ade3856c57fb32d948e28425930a81
Gui Reis <gui.sreis25@gmail.com>
2026-05-19 02:42:28 -0300

    Criando MainWindows e Home apartadas

--- maria_cacau/__main__.py
+++ maria_cacau/__main__.py
@@ -9,7 +9,7 @@ from PyQt6.QtWidgets import QApplication
 from maria_cacau import __app_name__, __icon_mac__, __icon_win__
 from maria_cacau.backend._server import BackendServer
 from maria_cacau.core.network import LocalClient, configure
-from maria_cacau.features.home.home_view import GuiMain
+from maria_cacau.features.main import MainWindow
 
 
 def main():
@@ -25,7 +25,7 @@ def main():
     icon_path = __icon_win__ if sys.platform == 'win32' else __icon_mac__
     app.setWindowIcon(QIcon(icon_path))
 
-    win = GuiMain()
+    win = MainWindow()
     win.show()
     sys.exit(app.exec())
 
