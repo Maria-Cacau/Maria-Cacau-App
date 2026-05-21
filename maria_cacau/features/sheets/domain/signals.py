cbe2c5992f1092872f099bb6fded3ba34c06e7eb
Gui Reis <gui.sreis25@gmail.com>
2026-05-21 00:46:58 -0300

    Limpeza de código (#61)
    * Removendo código lixo
    
    * Update .gitignore
    
    * Att doc

--- maria_cacau/features/sheets/domain/signals.py
+++ maria_cacau/features/sheets/domain/signals.py
@@ -4,10 +4,7 @@ from PyQt6.QtCore import QObject, pyqtSignal
 
 
 class SheetsSignals(QObject):
-    sheet_connected = pyqtSignal(object)
-    sheet_selected  = pyqtSignal(object)
-    sheet_renamed   = pyqtSignal(object)
-    error           = pyqtSignal(object)
+    error = pyqtSignal(object)
 
 
 signals: Final = SheetsSignals()
